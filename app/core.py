c2302504ff914eee2c8f103f6e2afd907a29d9d2
Peter gottlieb <gotped17@htl-kaindorf.at>
2021-10-13 22:30:34 +0200

    ka was das ist

--- app/core.py
+++ app/core.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, request
 from flask import render_template
+import random
 
 from coap_handler import coap_handler
-app = Flask(__name__, template_folder="./app/templates/")
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
+value = 0
 coap = coap_handler('cisco', '1vB3D29hz7Uja2wS', '192.168.1.10')
 @app.route('/', methods=['GET'])
 def home():
@@ -28,6 +30,7 @@ def lamp_light_change():
         coap.change_level(level)
     except:
         print('Not a number between 1 and 254')
+    print("deja vu")
     return redirect("http://localhost:5000/")
 
 @app.route('/api/moisturedata', methods=['PUT', 'GET'])
@@ -36,5 +39,10 @@ def get_moisture():
     if value <= 60:
         coap.turn_on()
     return '{"result": "success"}'
+
+@app.route('/api/moistureout')
+def return_moisture():
+    return '{"moisture": "' + str(random.randint(0,10)) + '"}'
+
 app.run(host = "0.0.0.0")
 
