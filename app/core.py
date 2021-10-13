df8ff6093f6e9ebfe61dc753ac10e6a2b14e1301
Peter gottlieb <gotped17@htl-kaindorf.at>
2021-10-13 22:26:59 +0200

    Added moistureout endpoint, needs to be changed for production use. Changed js so webpage dynamically sends request and shows moisture

---
+++ app/core.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, request
+from flask import render_template
+import random
+
+from coap_handler import coap_handler
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+value = 0
+coap = coap_handler('cisco', '1vB3D29hz7Uja2wS', '192.168.1.10')
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+app.add_url_rule("/", "index", home)
+
+@app.route('/api/off', methods=['PUT', 'GET'])
+def lamp_off():
+    coap.turn_off()
+    return redirect("http://localhost:5000/")
+
+@app.route('/api/on', methods=['PUT', 'GET'])
+def lamp_on():
+    coap.turn_on()
+    return redirect("http://localhost:5000/")
+@app.route('/api/on/level')
+def lamp_light_change():
+    try:
+        level = int(request.args['lightlevel'])
+        coap.change_level(level)
+    except:
+        print('Not a number between 1 and 254')
+    print("deja vu")
+    return redirect("http://localhost:5000/")
+
+@app.route('/api/moisturedata', methods=['PUT', 'GET'])
+def get_moisture():
+    value = int(request.args['moisture'])
+    if value <= 60:
+        coap.turn_on()
+    return '{"result": "success"}'
+
+@app.route('/api/moistureout')
+def return_moisture():
+    return '{"moisture": "' + str(random.randint(0,10)) + '"}'
+
+app.run(host = "0.0.0.0")
+
---
+++ app/static/main.js
@@ -0,0 +1,12 @@
+window.onload = () => { 
+  
+  setInterval(function () {
+    fetch('/api/moistureout', {
+      method: 'GET'})
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data)
+        document.getElementById('moisturefield').innerHTML=data.moisture;
+      })
+  }, 10000)
+};
