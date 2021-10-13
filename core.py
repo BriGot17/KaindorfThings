25686ec681e4bb8a01302d38ebfa07be46e17d00
Peter gottlieb <gotped17@htl-kaindorf.at>
2021-10-13 17:29:52 +0200

    Added data endpoint

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, request
 from flask import render_template
 
 from coap_handler import coap_handler
-app = Flask(__name__, template_folder="./src/templates")
+app = Flask(__name__, template_folder="./app/templates/")
 app.config["DEBUG"] = True
 
 coap = coap_handler('cisco', '1vB3D29hz7Uja2wS', '192.168.1.10')
@@ -29,6 +29,12 @@ def lamp_light_change():
     except:
         print('Not a number between 1 and 254')
     return redirect("http://localhost:5000/")
-    
+
+@app.route('/api/moisturedata', methods=['PUT', 'GET'])
+def get_moisture():
+    value = int(request.args['moisture'])
+    if value <= 60:
+        coap.turn_on()
+    return '{"result": "success"}'
 app.run(host = "0.0.0.0")
 
