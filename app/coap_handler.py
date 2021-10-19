bf82a92c7f085f9a3de0f07e8dcf7d2d46f695db
Peter gottlieb <gotped17@htl-kaindorf.at>
2021-10-19 09:15:38 +0200

    Deleted discomode because it bricked the gateway

--- app/coap_handler.py
+++ app/coap_handler.py
@@ -6,44 +6,38 @@ class coap_handler:
     name = ''
     key = ''
     ip = ''
+    
 
     def __init__(self, name, key, ip):
         self.name = name
         self.key = key
         self.ip = ip
+        pass
 
     def turn_on(self):
+        print('on')
         order = '{"5850": 1 }'
-        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131076"'
+        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131073"'
         stream = os.popen(command)
+        print(stream)
+        pass
 
     def change_level(self, level):
+        print('change level')
         lightlevel = f'"5851": {level}'
         order = '{"5850": 1, <place>}'
         order = order.replace('<place>', lightlevel)
-        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131076"'
+        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131073"'
         stream = os.popen(command)
         print(stream)
+        pass
     
     def turn_off(self):
-        print('hello')
+        print('off')
         order = '{"5850": 0}'
-        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131076"'
+        command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order}\' "coaps://{self.ip}:5684/15004/131073"'
         stream = os.popen(command)
         print(stream)
+        pass
 
-    def ciscodisco(self):
-        order1 = '{"5850": 0}'
-        order2 = '{"5850": 1, "5851": 254}'
-        order3 = '{"5850": 1, "5851": 10}'
-
-
-        #command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order1}\' "coaps://{self.ip}:5684/15004/131076"'
-        print('Hello1')
-        time.sleep(5)
-        print('Hello2')
-        #command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order2}\' "coaps://{self.ip}:5684/15004/131076"'
-        time.sleep(5)
-        print('Hello3')
-        #command = f'coap-client -m put -u "{self.name}" -k {self.key} -e \'{order3}\' "coaps://{self.ip}:5684/15004/131076"'
-        time.sleep(5)
+        
--- app/core.py
+++ app/core.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 value = 0
-coap = coap_handler('cisco', '1vB3D29hz7Uja2wS', '192.168.1.10')
+coap = coap_handler('cisco', 'vKhOxPvuAOysHDhm', '192.168.1.10')
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
@@ -15,13 +15,16 @@ app.add_url_rule("/", "index", home)
 
 @app.route('/api/off', methods=['PUT', 'GET'])
 def lamp_off():
+    print('api off')
     coap.turn_off()
     return redirect("http://localhost:5000/")
 
 @app.route('/api/on', methods=['PUT', 'GET'])
 def lamp_on():
+    print('api on')
     coap.turn_on()
     return redirect("http://localhost:5000/")
+
 @app.route('/api/on/level', methods=['PUT', 'GET'])
 def lamp_light_change():
     try:
@@ -46,14 +49,8 @@ def return_moisture():
     f = open('value.txt', 'r')
     value = f.read()
     f.close()
-    
     return '{"moisture": "' + str(value) + '"}'
 
-@app.route('/api/ciscodisco/on', methods=['PUT', 'GET'])
-def disco_on():
-    print('corehello')
-    coap.ciscodisco()
-    return 'Success'
 
 
 app.run(host = "0.0.0.0")
